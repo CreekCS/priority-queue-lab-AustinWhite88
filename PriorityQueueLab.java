437e2c10875bfcd17edd6b627a849c1107988c68
AustinWhite88 <130946233+AustinWhite88@users.noreply.github.com>
2024-11-05 20:43:35 +0000

    done!

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -1,6 +1,37 @@
 // import statements
+import java.io.*;
+import java.util.*;
 
 public class PriorityQueueLab {
-  
+    public static void main(String[]args)
+    {
+        Scanner scan = null;
+        PriorityQueue<Integer> pQueue = new PriorityQueue<Integer>();
+
+        try{
+        
+        
+            //scan file
+            scan = new Scanner(new File("RandIntegers.txt"));
+
+            //add ints from scanned file to pqueue
+            while(scan.hasNextInt()){
+                pQueue.add(scan.nextInt());
+            }
+        
+
+    
+        }catch(FileNotFoundException e){
+            System.out.println("yo bih broke for a bit"+ e);
+        }catch(Exception e){
+
+            System.out.println("ERROR: "+e);
+        }
+
+        while(!pQueue.isEmpty()){
+            System.out.println(pQueue.remove() + "\n");
+        }
+        //System.out.println(pQueue);
+}
 }
 
